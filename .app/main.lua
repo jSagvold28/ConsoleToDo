6914c42495891a585789d13bfdb8e6f3c3d9d16f
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2023-01-18 17:34:51 -0600

    not complete - save

---
+++ .app/main.lua
@@ -0,0 +1,35 @@
+local todo = {}
+
+local function addTask()
+
+    io.write("Task name: ")
+
+    -- Optinal fields of input
+
+    io.write("Date want completed by: ")
+
+    io.write("Time: ")
+
+    io.write("Urgency: ")
+
+    io.write("Assignees: ")
+
+    io.write("Other notes: ")
+
+
+    local taskName = io.read()
+    local Time = io.read()
+    local Urgency = io.read()
+    local Assignees = io.read()
+    local otherNotes = io.read()
+
+
+    local input == io.read()
+
+    print("Would you like to create a text file of this task?")
+    print("Type 'yes' to create a file. Type 'no', or nothing to not create a text file.")
+
+    -- make a if statment here
+
+
+end
